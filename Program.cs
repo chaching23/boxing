9512140e12f4438631b1c16a1e23e1aa2b6b5646
kian chachi <chachi@kians-MacBook-Pro.local>
2019-08-13 13:34:37 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> ice = new List<object>();
+            ice.Add(7);
+            ice.Add(28);
+            ice.Add(-1);
+            ice.Add(true);
+            ice.Add("chair");
+
+            for(int i = 0; i < ice.Count; i++){
+
+            System.Console.WriteLine(ice[i]);
+
+            }
+
+          
+            
+        }
+
+
+
+
+
+
+
+    }
+}
